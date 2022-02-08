c0813ae159399773d9276ea78bd9192db79f518f
Chandler Klein <kleinc16@gmail.com>
2022-02-07 23:22:03 -0500

    Updated Adapter Pattern

---
+++ src/main/java/com/chandler/patterns/adapter/DuckAdapter.java
@@ -0,0 +1,19 @@
+package com.chandler.patterns.adapter;
+
+public class DuckAdapter implements Turkey {
+    Duck duck;
+
+    public DuckAdapter(Duck duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void gobble() {
+        duck.quack();
+    }
+
+    @Override
+    public void fly() {
+        duck.fly();
+    }
+}
--- src/main/java/com/chandler/patterns/adapter/DuckTestDrive.java
+++ src/main/java/com/chandler/patterns/adapter/DuckTestDrive.java
@@ -4,6 +4,7 @@ public class DuckTestDrive {
 
     public static void main(String[] args) {
         var duck = new MallardDuck();
+        Turkey duckAdapter = new DuckAdapter(duck);
 
         var turkey = new WildTurkey();
         Duck turkeyAdapter = new TurkeyAdapter(turkey);
@@ -17,6 +18,14 @@ public class DuckTestDrive {
 
         System.out.println("\nThe TurkeyAdapter says...");
         testDuck(turkeyAdapter);
+
+        System.out.println("\n The DuckAdapter says...");
+        testTurkey(duckAdapter);
+    }
+
+    private static void testTurkey(Turkey duckAdapter) {
+        duckAdapter.gobble();
+        duckAdapter.fly();
     }
 
     private static void testDuck(Duck duck) {
